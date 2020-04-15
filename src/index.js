8f85ae8c52e797c2b7ac77df4b25dd3b2be9dead
Palmi Moller <palmimoller@me.com>
2020-04-15 16:12:27 -0600

    Passing unique data for each post with props

--- src/index.js
+++ src/index.js
@@ -8,11 +8,24 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <CommentDetail
+        author="Sam"
+        timeAgo="today at 4:45PM"
+        body="I'm a Fan!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="today at 2:00AM"
+        body="It's pretty good"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="yesterday at 5:00PM"
+        body="You make good points"
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
